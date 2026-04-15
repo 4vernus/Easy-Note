de450c122ace10b300dd726afc4ccf02d1d10718
4vernus <samir.abdullayevv05@gmail.com>
2026-04-15 20:53:59 +0400

    output ready

--- src/output.py
+++ src/output.py
@@ -1,1 +1,14 @@
-# Writes extracted key points to a .txt file
+# Writes extracted key points to a .md file
+from pathlib import Path
+
+def write_output(input_file,output):
+    dst_folder = Path("outputs")
+    dst_folder.mkdir(exist_ok=True)
+    filename = f"{input_file.stem}.md"
+    dst_path = dst_folder / filename
+
+    with open(dst_path, "w") as f:
+        f.write(output)
+    
+    return dst_path
+
